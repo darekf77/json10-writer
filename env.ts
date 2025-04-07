148bff6d0e38967e15218133452f51fbff794ae5
Dariusz Filipiak <darekf77@gmail.com>
2025-04-07 17:07:22 +0200

    release: v18.0.21

---
+++ env.ts
@@ -0,0 +1,9 @@
+import type { EnvOptions } from 'tnp/src';
+
+const env: Partial<EnvOptions> = {
+  website: {
+    domain: 'json10-writer.example.domain.com',
+    title: 'Json 10 Writer',
+  },
+};
+export default env;
